c28830b5e6fdff9fe569c71aad5bf9fe8422400d
PembangunanSIG <87271905+PembangunanSIG@users.noreply.github.com>
2021-10-18 21:47:57 +0700

    Add files via upload

---
+++ 1-1.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("Salam Budi Luhur!");
+    return 0;
+}
---
+++ Tugas 1-5 2.cpp
@@ -0,0 +1,13 @@
+#include "stdio.h"
+int main()
+{
+    int P, L;
+    P = 10;
+    L = 7;
+    printf("Rumus Luas Persegi Panjang adalah a x t \n\n");
+    printf("10 x 7 = ");
+    printf("%i", (L*P));
+    printf(" cm");
+    return 0;
+}
+
